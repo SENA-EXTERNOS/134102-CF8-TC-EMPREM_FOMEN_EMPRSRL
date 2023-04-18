2c6cd13bde40fb44cad1112fd23ff0c2b5147a63
Andrea Botello <andrea.botellor@misena.edu.co>
2023-04-18 16:50:45 -0500

    Videos

--- src/views/Tema1.vue
+++ src/views/Tema1.vue
@@ -12,8 +12,9 @@
 
     figure.mb-5
       .video
-        iframe(width="560" height="315" src="https://www.youtube.com/embed/2L91WMqw96A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
-      figcaption Video: Leyenda del video
+        iframe(width="560" height="315" src="https://www.youtube.com/embed/UkbOeuNM8YY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+
+
 
 
     ul.lista-ul--color.mb-3
--- src/views/Tema4.vue
+++ src/views/Tema4.vue
@@ -245,7 +245,8 @@
     figure.mb-5
         .video
           iframe(width="560" height="315" src="https://www.youtube.com/embed/2L91WMqw96A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
-        figcaption Video: Leyenda del video
+
+
 
     p.mb-5 La Guía Técnica Colombiana GTC 185, plantea las siguientes características de redacción y presentación, que se deben considerar en la elaboración de las actas:
 
